c0330450227bbaa2f1ab27ff2ef37572a6467957
Vinicios Neves <vinicios_neves@hotmail.com>
2021-12-14 22:40:53 +0000

    fim aula 2.3

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -5,20 +5,39 @@
       Você não está muito produtivo hoje
       <span class="has-text-weight-bold">:(</span>
     </Box>
-    <Tarefa v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index" />
-    <div class="modal is-active">
+    <Tarefa
+      v-for="(tarefa, index) in tarefas"
+      :tarefa="tarefa"
+      :key="index"
+      @aoTarefaClicada="selecionarTarefa"
+    />
+    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
-          <p class="modal-card-title">Modal title</p>
-          <button class="delete" aria-label="close"></button>
+          <p class="modal-card-title">Editando uma tarefa</p>
+          <button
+            @click="fecharModal"
+            class="delete"
+            aria-label="close"
+          ></button>
         </header>
         <section class="modal-card-body">
-          <!-- Content ... -->
+          <div class="field">
+            <label for="descricaoDaTarefa" class="label">
+              Descrição
+            </label>
+            <input
+              type="text"
+              class="input"
+              v-model="tarefaSelecionada.descricao"
+              id="descricaoDaTarefa"
+            />
+          </div>
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success">Save changes</button>
-          <button class="button">Cancel</button>
+          <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
+          <button @click="fecharModal" class="button">Cancelar</button>
         </footer>
       </div>
     </div>
@@ -32,6 +51,7 @@ import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
 import { useStore } from "@/store";
 import {
+  ALTERAR_TAREFA,
   CADASTRAR_TAREFA,
   OBTER_PROJETOS,
   OBTER_TAREFAS,
@@ -45,10 +65,25 @@ export default defineComponent({
     Tarefa,
     Box,
   },
+  data() {
+    return {
+      tarefaSelecionada: null as ITarefa | null,
+    };
+  },
   methods: {
     salvarTarefa(tarefa: ITarefa): void {
       this.store.dispatch(CADASTRAR_TAREFA, tarefa);
     },
+    selecionarTarefa(tarefa: ITarefa) {
+      this.tarefaSelecionada = tarefa;
+    },
+    fecharModal() {
+      this.tarefaSelecionada = null;
+    },
+    alterarTarefa () {
+      this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => this.fecharModal())
+    }
   },
   computed: {
     semTarefas(): boolean {
