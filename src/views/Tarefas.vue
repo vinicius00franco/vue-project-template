4bce2e66a9792613a9e36cb6bf372db7f6f87e14
Vinicios Neves <vinicios_neves@hotmail.com>
2021-12-26 21:28:11 +0000

    video 4.1

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -5,13 +5,30 @@
       Você não está muito produtivo hoje
       <span class="has-text-weight-bold">:(</span>
     </Box>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input
+          class="input"
+          type="text"
+          placeholder="Digite para filtrar"
+          v-model="filtro"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa
       v-for="(tarefa, index) in tarefas"
       :tarefa="tarefa"
       :key="index"
       @aoTarefaClicada="selecionarTarefa"
     />
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+    <div
+      class="modal"
+      :class="{ 'is-active': tarefaSelecionada }"
+      v-if="tarefaSelecionada"
+    >
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
@@ -24,9 +41,7 @@
         </header>
         <section class="modal-card-body">
           <div class="field">
-            <label for="descricaoDaTarefa" class="label">
-              Descrição
-            </label>
+            <label for="descricaoDaTarefa" class="label"> Descrição </label>
             <input
               type="text"
               class="input"
@@ -36,7 +51,9 @@
           </div>
         </section>
         <footer class="modal-card-foot">
-          <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
+          <button @click="alterarTarefa" class="button is-success">
+            Salvar alterações
+          </button>
           <button @click="fecharModal" class="button">Cancelar</button>
         </footer>
       </div>
@@ -45,7 +62,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -80,10 +97,11 @@ export default defineComponent({
     fecharModal() {
       this.tarefaSelecionada = null;
     },
-    alterarTarefa () {
-      this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
-        .then(() => this.fecharModal())
-    }
+    alterarTarefa() {
+      this.store
+        .dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => this.fecharModal());
+    },
   },
   computed: {
     semTarefas(): boolean {
@@ -94,9 +112,19 @@ export default defineComponent({
     const store = useStore();
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
+
+    const filtro = ref("");
+
+    const tarefas = computed(() =>
+      store.state.tarefa.tarefas.filter(
+        (t) => !filtro.value || t.descricao.includes(filtro.value)
+      )
+    );
+
     return {
-      tarefas: computed(() => store.state.tarefa.tarefas),
+      tarefas,
       store,
+      filtro,
     };
   },
 });
